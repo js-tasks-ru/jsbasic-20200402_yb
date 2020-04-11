da7a8d19f83dbbebe3aff07a86b44c77bb27c7dd
Yusef Benarieb <youssefgothic2@gmail.com>
2020-04-11 16:31:22 +0300

    solved task 1.1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,4 +5,20 @@
  */
 function factorial(n) {
   // ваш код...
+
+let count = n - 1;
+
+if ( (n == 0) || (n == 1) ) {
+  return(1)
+}
+
+do {
+  n = n * count;
+  count -= 1;
+}
+while (count > 1);
+console.log(n);
+return(n);
+
+
 }
