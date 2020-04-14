8a17aa787c6b81736b401b2d59f2f6d4955e3c80
Yusef Benarieb <youssefgothic2@gmail.com>
2020-04-14 11:56:10 +0300

    solved tasks from 1.2 to 1.5

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,6 +3,13 @@
  * @param {string} str
  * @returns {string}
  */
-function ucFirst(str) {
-  // ваш код...
-}
+function ucFirst (str){
+
+  if (str) {
+      return str = str[0].toUpperCase() + str.slice(1, str.length)
+     } 
+     else return str; 
+ 
+   }
+ 
+      
